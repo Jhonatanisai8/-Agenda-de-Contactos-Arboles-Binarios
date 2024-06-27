30e8a394104cc4bf93c43b670e4f535e770fba9a
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-26 21:17:47 -0500

    clase arbol-Contacto: método para agregar un contacto al arbol

--- src/main/java/org/jhonatan/app/Modelo/ArbolContacto.java
+++ src/main/java/org/jhonatan/app/Modelo/ArbolContacto.java
@@ -16,4 +16,18 @@ public class ArbolContacto {
         this.raiz = raiz;
     }
 
+    public NodoContacto agregarContacto(NodoContacto nodoContacto, Contacto contacto) {
+        if (nodoContacto == null) {
+            NodoContacto nuevo = new NodoContacto(contacto);
+            return nuevo;
+        } else {
+            if (contacto.getNombreCompleto().compareTo(nodoContacto.getContacto().getNombreCompleto()) > 0) {
+                nodoContacto.setDere(agregarContacto(nodoContacto.getDere(), contacto));
+            } else {
+                nodoContacto.setIzq(agregarContacto(nodoContacto.getIzq(), contacto));
+            }
+        }
+        return nodoContacto;
+    }
+
 }
