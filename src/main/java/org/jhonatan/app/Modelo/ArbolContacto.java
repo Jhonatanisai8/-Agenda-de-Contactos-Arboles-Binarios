0bfe6a31f893bc0518c025c38ae426f5ed43c832
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-26 21:12:52 -0500

    clase arbol-Contacto: atributos,constructor,geters y setters

---
+++ src/main/java/org/jhonatan/app/Modelo/ArbolContacto.java
@@ -0,0 +1,19 @@
+package org.jhonatan.app.Modelo;
+
+public class ArbolContacto {
+
+    private NodoContacto raiz;
+
+    public ArbolContacto() {
+        raiz = null;
+    }
+
+    public NodoContacto getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoContacto raiz) {
+        this.raiz = raiz;
+    }
+
+}
