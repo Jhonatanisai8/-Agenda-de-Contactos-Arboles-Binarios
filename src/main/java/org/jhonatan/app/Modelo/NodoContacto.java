419cb57c25b9a0306ad5433017b63f4e81ebac6c
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-26 21:06:43 -0500

    clase nodo-contacto: getters y setters

--- src/main/java/org/jhonatan/app/Modelo/NodoContacto.java
+++ src/main/java/org/jhonatan/app/Modelo/NodoContacto.java
@@ -11,4 +11,28 @@ public class NodoContacto {
         dere = null;
     }
 
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+
+    public NodoContacto getIzq() {
+        return izq;
+    }
+
+    public void setIzq(NodoContacto izq) {
+        this.izq = izq;
+    }
+
+    public NodoContacto getDere() {
+        return dere;
+    }
+
+    public void setDere(NodoContacto dere) {
+        this.dere = dere;
+    }
+
 }
