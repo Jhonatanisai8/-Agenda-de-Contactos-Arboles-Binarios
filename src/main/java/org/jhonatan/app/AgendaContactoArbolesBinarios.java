552e9690774121b3c097d0cd3308f10993df76b5
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-26 21:56:18 -0500

    clase main: llamando al formulario principal

--- src/main/java/org/jhonatan/app/AgendaContactoArbolesBinarios.java
+++ src/main/java/org/jhonatan/app/AgendaContactoArbolesBinarios.java
@@ -1,8 +1,11 @@
 package org.jhonatan.app;
 
+import org.jhonatan.app.Vista.frmPrincipal;
+
 public class AgendaContactoArbolesBinarios {
 
     public static void main(String[] args) {
-
+        frmPrincipal v1 = new frmPrincipal();
+        v1.setVisible(true);
     }
 }
